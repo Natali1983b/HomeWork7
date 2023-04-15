624d412bb84b55464c2bd6e35a7a58c7433d6c9e
Natali <nbyka@mail.ru>
2023-04-15 21:28:20 +0300

    2MerniiMassiveOfDouble

---
+++ Task47_2MerniiMassiveOfDouble/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double [,] CreateArray (int length1, int lenght2)
+{
+    double [,] Array = new double [length1, lenght2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           Array[i,j] = new Random().NextDouble() * 1000;
+        }
+    }
+    return Array;
+}
+
+void PrintArray(double [,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           System.Console.Write(Array[i,j]+" ");
+           
+        }
+        System.Console.WriteLine();
+    }
+}
+
+PrintArray(CreateArray(7, 5));
+
